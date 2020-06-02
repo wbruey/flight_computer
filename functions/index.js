ad1639a7b5b4a2226db79116a1eb8cfe0f04e4ba
Will Bruey <willbruey@gmail.com>
2020-06-02 17:56:05 -0600

    first commit for flight comptuer i just set up the environment and for the first time got the function emulator working which is cool because it successfully calls the twilo interface

---
+++ functions/index.js
@@ -0,0 +1,51 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+const request = require('request');
+const language = require('@google-cloud/language');
+const cors = require('cors')({
+  origin: true,
+});
+var twilio = require('twilio');
+const twilio_accountSid = 'AC0ffe354bb1064b240f2c38ae8eef3744';
+const twilio_authToken = '2c5c388e7ba125a02e290eeb51cca88e';
+var http = require('http');
+const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+
+
+//===========TEST FUNCTION======================================================================
+//{ INVITE A NEW FRIEND also puts  unique_id into user phone number structure 
+//https://us-central1-brutest2-62192.cloudfunctions.net/textMeats
+exports.test_text = functions.pubsub.schedule('every 2 minutes').onRun((context) => {
+	// Grab the unique_id parameter from the GET request
+    //unique_id=String(Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15));
+    var phone = '7174756561';
+  	//console.log('New user ' + phone+' created with unique id: '+unique_id);
+	const client = require('twilio')(twilio_accountSid, twilio_authToken);
+
+
+//	console.log('hello');
+
+	var today = new Date();
+	var delivery_day = new Date('December 12, 2020 03:24:00');
+	var days_left=String(Math.floor((delivery_day-today)/1000/60/60/24));
+	
+	client.messages.create(
+	  {
+		to: '+1'+phone,
+		from: '+17176960783',
+		//body: 'http://www.apple.com/iphone',
+		body: days_left+' days til BABY TIME!!!! yay!',
+	  },
+	  (err, message) => {
+		console.log(message.sid);
+	  }
+	);
+	return{
+		message: 'thanks',
+	};
+	
+	
+});
+//}
