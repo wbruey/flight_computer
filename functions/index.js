3b1e2b8a8654622701ac0053819943bcaf4cdeaa
Will Bruey <willbruey@gmail.com>
2020-06-04 11:19:35 -0600

    in the middle of testing but i got hacked again because i forgot to add the local runtime env config to gitignore

--- functions/index.js
+++ functions/index.js
@@ -13,11 +13,121 @@ const readline = require('readline');
 const {google} = require('googleapis');
 const request = require('request-promise');
 
+//=======LEARNING ABOUT PROMISES===============
+exports.promise_test = functions.pubsub.schedule('every 60 minutes').onRun((context) => { 
+
+	function fake_grab(text1,text2){
+		functions_promise = new Promise((resolve,reject)=>{
+			if(text1.length && text2.length){
+				resolve(text1 +'   ' +  text2);
+			}else{
+				throw new Error("one of the texts is blank");
+			}
+		});
+		return functions_promise;
+	}
+
+
+	const googles_promise = fake_grab('',' breast')  // define the promise
+		.then((text_data) => {     //define what to do next if promise is fulfilled
+			console.log(text_data);
+		})
+		.catch((err)=> {
+			console.log(err);  //define what to do i fthe promise is not fulfilled.
+			console.log('do a top level error thing');
+			
+		});
+
+	setTimeout(()=>console.log('do an adjacent thing'),1000);
+	
+	
+	
+	return 0;
+
+});
+
+//returns a promise to grab spreadsheet data from google API given a spreadsheet ID and a range to grab data from
+function grab_spreadsheet_data(spreadsheetId,range){
+	//construct the promise to return
+	const googles_promise = new Promise((resolve,reject)=>{
+		const sheets = google.sheets({version: 'v4', auth: functions.config().google_auth.api_key});  //create a google sheets object interface object with my api key
+		sheets.spreadsheets.values.get({  //use that api object to get spreadsheet data in a spreadsheet at a range
+			spreadsheetId: spreadsheetId,
+			range: range,
+		}, (err, res) => {
+			if (err){
+				console.log('Google API didnt work');
+				throw new Error(err);
+			} else {
+				const rows = res.data.values;
+				if (rows.length) {
+				  resolve(rows);
+				} else {
+				  console.log('Sheets API worked but no (blank) data found at google sheet.');
+				}
+			}
+		});
+	});	
+	return googles_promise;
+}
+
+//returns a promise to send a text message and then hopefully sends it
+function send_text_message(phone,text_message){
+	//construct the promise to return
+	const twilios_promise = new Promise((resolve,reject)=>{
+		const client = require('twilio')(functions.config().twilio_auth.account_sid, functions.config().twilio_auth.auth_token); //create a client api connection object 
+		client.messages.create(  //create a message and send it.
+		  {
+			to: '+1'+phone,
+			from: '+17176960783', // this is my twilio number
+			body: text_message,  // this the text in the sms message
+		  },
+		  function(err,message){  // if there is a return message from twilio grab it. 
+			  if(err){
+				  console.log('Twilio API didnt work');
+				  throw new Error(err);  //if there is an error in that message, log it .
+			  }else{
+				  console.log('Twilio sent a text message with sid recepit: ' + message.sid);
+				  resolve(message.sid);
+			  }
+		  }
+		);	
+	});
+	return twilios_promise;
+}
 
 
 
 //=======TEXT WILL TOTAL MAINTENACE SUMMARY===============
-exports.text_maintenance_summary = functions.pubsub.schedule('every 2 minutes').onRun((context) => { 
+exports.text_maintenance_summary2 = functions.pubsub.schedule('every 60 minutes').onRun((context) => { 
+	
+	//define the spreadsheet to get data from and the phone number to send the text too.
+	const spreadsheetId='1drcYhNrzV9IPNpCUqKCbhdVfr3wlQ-eW8p_zujWRMu0';
+	const range='routine_time!B1:C2';
+	const phone = '7174756561';
+	var text_message = 'error with text_maintenance_summary firebase function'; // by default the text message should say an error since it's supposed to get updated below.
+	
+	//get a promise to do the things (grab data and then text) then do them.
+	const promise_to_text_maintenance_status = grab_spreadsheet_data(spreadsheetId,range)
+		//then format the data from the spreadsheeet to create the message you want to send.  remember, youre passing a function to the .then method slot.
+		.then((rows) =>{
+			text_message='Today is ' + rows[0][0] + ' '+ rows[0][1] + '. You have ' + rows[1][1] + ' days left until you are late on plane maintence.';
+			console.log('Text body will be: ' + text_message)
+			//then return the PROMISE of a text message which will be the promise for the next THEN in the chain...
+			return send_text_message(phone,text_message);
+		})
+		//after the promise of sending a text message has completed...
+		.then(() => console.log('Successfully Updated Will with Maintenance Report'))
+		//if there was an error, print it to console.
+		.catch(err=>console.log('Failed to update Will with Maintenance Report:  ' + err));
+
+	// make highest level firebase function happy
+	return 0;
+});
+
+
+//=======TEXT WILL TOTAL MAINTENACE SUMMARY===============
+exports.text_maintenance_summary = functions.pubsub.schedule('every 60 minutes').onRun((context) => { 
 	//define the phone number to send the text too.
 	var phone = '7174756561';
 	var console_message='';
